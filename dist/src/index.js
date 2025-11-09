89d42fec539455f2e4c80f8bca8a1e12054f16a7
Ikenna <emeruchecole9@gmail.com>
2025-11-09 23:23:25 +0100

    qwewfd

--- dist/src/index.js
+++ dist/src/index.js
@@ -7,29 +7,27 @@ import { launch } from 'puppeteer';
 import showdown from 'showdown';
 import showdownEmoji from 'showdown-emoji';
 import showdownHighlight from 'showdown-highlight';
-import { fileURLToPath } from 'url';
 import { getOptions } from './puppeteer-helper.js';
 import { getStyleBlock, getStyles, qualifyImgSources } from './utils.js';
 const { setFlavor, Converter } = showdown;
 const { SafeString, compile } = handlebars;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 // Main layout template
 const layoutPath = join(__dirname, '/layouts/doc-body.hbs');
 const headerLayoutPath = join(__dirname, '/layouts/header.hbs');
 const footerLayoutPath = join(__dirname, '/layouts/footer.hbs');
 function getAllStyles(options) {
     let cssStyleSheets = [];
+    console.log(import.meta.url, __filename, __dirname);
     // GitHub Markdown Style
-    // if (options.ghStyle) {
-    //   cssStyleSheets.push(join(__dirname, '/assets/github-markdown-css.css'));
-    // }
-    // // Highlight CSS
-    // cssStyleSheets.push(join(__dirname, '/assets/highlight/styles/github.css'));
-    // // Some additional defaults such as margins
-    // if (options.defaultStyle) {
-    //   cssStyleSheets.push(join(__dirname, '/assets/default.css'));
-    // }
+    if (options.ghStyle) {
+        cssStyleSheets.push(join(__dirname, '/assets/github-markdown-css.css'));
+    }
+    // Highlight CSS
+    cssStyleSheets.push(join(__dirname, '/assets/highlight/styles/github.css'));
+    // Some additional defaults such as margins
+    if (options.defaultStyle) {
+        cssStyleSheets.push(join(__dirname, '/assets/default.css'));
+    }
     // Optional user given CSS
     if (options.styles) {
         cssStyleSheets = cssStyleSheets.concat(options.styles);
